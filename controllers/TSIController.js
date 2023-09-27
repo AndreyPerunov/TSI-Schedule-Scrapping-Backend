ad2b533e2cffd53c060ea2293cf8605d749479e0
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-09-27 15:37:13 +0300

    Added log

--- controllers/TSIController.js
+++ controllers/TSIController.js
@@ -3,6 +3,7 @@ import TSI from "../models/TSI.js"
 class TSIController {
   async getSchedule(req, res) {
     try {
+      console.log("/api/schedule")
       const schedule = await TSI.getSchedule(req.body.group, req.body.days)
       res.json(schedule)
     } catch (e) {
