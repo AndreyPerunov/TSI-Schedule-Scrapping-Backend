a5aabd1698572afc856e417c8ba0cd885420ccc7
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-05 09:44:36 +0300

    Added home page

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.use(express.json())
 
 app.use("/api", router)
 
+app.get("/", (req, res) => {
+  res.send(`
+  <h1>TSI Schedule Scrapping Backend</h1>
+  <h2>Available routes:</h2>
+  <ul>
+    <li>
+      POST: <a href="${req.protocol + "://" + req.get("host")}/api/schedule">${req.protocol + "://" + req.get("host")}/api/schedule</a>
+    </li>
+  </ul>
+  `)
+})
+
 const PORT = process.env.NODE_ENV || 8080
 
 app.listen(PORT, () => {
