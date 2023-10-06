633f7b4972dced858dd890591fb23a47a3ca7aae
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-06 15:26:40 +0300

    removed get

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ app.use(express.json())
 
 app.use("/api", router)
 
-app.get("/", (req, res) => {
-  res.type("html").send(`
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Schedule Scrapper API's</title>
-    <script>
-      function clickScheduleHandler(e) {
-        e.preventDefault(); 
-        navigator.clipboard.writeText("${req.protocol + "://" + req.get("host")}/api/schedule");
-      }
-    </script>
-  </head>
-  <body>
-    <h1>TSI Schedule Scrapping Backend</h1>
-    <h2>Available routes:</h2>
-    <small>Click the link to copy it.</small>
-    <ul>
-      <li>
-        POST: <a href="/" onclick="clickScheduleHandler(event)">
-          ${req.protocol + "://" + req.get("host")}/api/schedule
-        </a>
-      </li>
-    </ul>
-  </body>
-</html>
-  `)
-})
+// app.get("/", (req, res) => {
+//   res.type("html").send(`
+// <!DOCTYPE html>
+// <html>
+//   <head>
+//     <title>Schedule Scrapper API's</title>
+//     <script>
+//       function clickScheduleHandler(e) {
+//         e.preventDefault();
+//         navigator.clipboard.writeText("${req.protocol + "://" + req.get("host")}/api/schedule");
+//       }
+//     </script>
+//   </head>
+//   <body>
+//     <h1>TSI Schedule Scrapping Backend</h1>
+//     <h2>Available routes:</h2>
+//     <small>Click the link to copy it.</small>
+//     <ul>
+//       <li>
+//         POST: <a href="/" onclick="clickScheduleHandler(event)">
+//           ${req.protocol + "://" + req.get("host")}/api/schedule
+//         </a>
+//       </li>
+//     </ul>
+//   </body>
+// </html>
+//   `)
+// })
 
 const PORT = process.env.NODE_ENV || 8080
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`)
 })
