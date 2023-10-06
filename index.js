fbc4214223cea616cfa500d9ce9b173cbe4e35ef
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-06 17:21:38 +0300

    finnaly fixed

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 import express from "express"
-// import router from "./router.js"
+import router from "./router.js"
 
 const app = express()
 
-// app.use(express.json())
+app.use(express.json())
 
-// app.use("/api", router)
+app.use("/api", router)
 
-// app.get("/", (req, res) => {
-//   res.type("html").send(`
-// <!DOCTYPE html>
-// <html>
-//   <head>
-//     <title>Schedule Scrapper API's</title>
-//     <script>
-//       function clickScheduleHandler(e) {
-//         e.preventDefault();
-//         navigator.clipboard.writeText("${req.protocol + "://" + req.get("host")}/api/schedule");
-//       }
-//     </script>
-//   </head>
-//   <body>
-//     <h1>TSI Schedule Scrapping Backend</h1>
-//     <h2>Available routes:</h2>
-//     <small>Click the link to copy it.</small>
-//     <ul>
-//       <li>
-//         POST: <a href="/" onclick="clickScheduleHandler(event)">
-//           ${req.protocol + "://" + req.get("host")}/api/schedule
-//         </a>
-//       </li>
-//     </ul>
-//   </body>
-// </html>
-//   `)
-// })
-
-app.get("/", (req, res) => res.send("Hi"))
+app.get("/", (req, res) => {
+  res.type("html").send(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Schedule Scrapper API's</title>
+    <script>
+      function clickScheduleHandler(e) {
+        e.preventDefault();
+        navigator.clipboard.writeText("${req.protocol + "://" + req.get("host")}/api/schedule");
+      }
+    </script>
+  </head>
+  <body>
+    <h1>TSI Schedule Scrapping Backend</h1>
+    <h2>Available routes:</h2>
+    <small>Click the link to copy it.</small>
+    <ul>
+      <li>
+        POST: <a href="/" onclick="clickScheduleHandler(event)">
+          ${req.protocol + "://" + req.get("host")}/api/schedule
+        </a>
+      </li>
+    </ul>
+  </body>
+</html>
+  `)
+})
 
 const PORT = process.env.PORT || 8080
 
