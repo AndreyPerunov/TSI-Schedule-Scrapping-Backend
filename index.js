818d2526bf589c704eb1ffec778cac64bf97fd89
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-06 13:34:48 +0300

    Improved homepage response

--- index.js
+++ index.js
@@ -10,23 +10,31 @@ app.use(express.json())
 app.use("/api", router)
 
 app.get("/", (req, res) => {
-  res.send(`
-  <script type="text/javascript">
-  function clickScheduleHandler(e) {
-    e.preventDefault(); 
-    navigator.clipboard.writeText("${req.protocol + "://" + req.get("host")}/api/schedule");
-  }
-  </script>
-  <h1>TSI Schedule Scrapping Backend</h1>
-  <h2>Available routes:</h2>
-  <small>Click the link to copy it.</small>
-  <ul>
-    <li>
-      POST: <a href="/" onclick="clickScheduleHandler(event)">
-        ${req.protocol + "://" + req.get("host")}/api/schedule
-      </a>
-    </li>
-  </ul>
+  res.type("html").send(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Schedule Scrapper API's</title>
+    <script>
+      function clickScheduleHandler(e) {
+        e.preventDefault(); 
+        navigator.clipboard.writeText("${req.protocol + "://" + req.get("host")}/api/schedule");
+      }
+    </script>
+  </head>
+  <body>
+    <h1>TSI Schedule Scrapping Backend</h1>
+    <h2>Available routes:</h2>
+    <small>Click the link to copy it.</small>
+    <ul>
+      <li>
+        POST: <a href="/" onclick="clickScheduleHandler(event)">
+          ${req.protocol + "://" + req.get("host")}/api/schedule
+        </a>
+      </li>
+    </ul>
+  </body>
+</html>
   `)
 })
 
