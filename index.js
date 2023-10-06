43be50dbc380567ef9f10fd9fe56a51b61c257ea
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-06 15:15:30 +0300

    Applied cors to api router

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express"
-import cors from "cors"
 import router from "./router.js"
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 
 app.use("/api", router)
--- router.js
+++ router.js
@@ -1,7 +1,10 @@
 import Router from "express"
 import TSIController from "./controllers/TSIController.js"
+import cors from "cors"
 
 const router = Router()
+
+router.use(cors())
 
 router.post("/schedule", TSIController.getSchedule)
 
